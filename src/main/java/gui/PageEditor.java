75e93c36da3b1e863935aab4435c772fea7a52d8
sijmen_v_b <sijmenvanbommel@gmail.com>
2022-09-12 23:52:51 +0200

    added nonfuctional page selector.

--- src/main/java/gui/PageEditor.java
+++ src/main/java/gui/PageEditor.java
@@ -8,14 +8,15 @@ import javafx.scene.text.Font;
 import textElement.PlainTextElement;
 
 public class PageEditor extends HBox {
+
 	// private InlineCssTextArea area;
 	private TextElementType type = TextElementType.TEXT;
 	private Book book;
-	
-	public PageEditor(Book book) {		
-		
+
+	public PageEditor(Book book) {
+		this.setSpacing(5);
 		PagePreview preview = new PagePreview(book.getPages().getFirst());
-		plainTextInput input = new plainTextInput(book.getPages().getFirst().getTextElements().getFirst(),preview);
+		TextInput input = new TextInput(book.getPages().getFirst().getTextElements().getFirst(), preview);
 
 		this.getChildren().addAll(preview, input);
 
--- src/main/java/gui/TextInput.java
+++ src/main/java/gui/TextInput.java
@@ -2,26 +2,44 @@ package gui;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import textElement.TextElement;
 
-public class plainTextInput extends VBox {
-	
+public class TextInput extends VBox {
+	private final int SPACING = 5;// how much space is between the different elements.
 	private TextArea input;
 	private TextElement currentElement;
 	private PagePreview previewRef;
 	
-	public plainTextInput(TextElement element,PagePreview previewRef) {
+	public TextInput(TextElement element,PagePreview previewRef) {
+		
+		this.setSpacing(SPACING);// set vertical spacing.
+		
+		Button previousButton = new Button("previous");
+		Label pageSeletorLabel = new Label(" page 1 of 1 ");
+		pageSeletorLabel.setFont(Font.font(16));
+		Button nextButton = new Button("next");
+		Button addPageButton = new Button("+");
+		Button moveLeftButton = new Button("move left");
+		Button moveRightButton = new Button("move Right");
+		HBox pageselector = new HBox(SPACING,moveLeftButton,previousButton,pageSeletorLabel,nextButton,addPageButton,moveRightButton);
+		
+		
 		currentElement = element;		
 		this.previewRef = previewRef;
 		ChoiceBox<String> typeSelector = new ChoiceBox<>();
 		typeSelector.getItems().add("text");
 		typeSelector.setValue("text");
+		
 
 		input = new TextArea();
 		
@@ -41,7 +59,7 @@ public class plainTextInput extends VBox {
 
 		ColorPicker colour = new ColorPicker(Color.BLACK);
 
-		this.getChildren().addAll(typeSelector, input, bold, italics, colour);
+		this.getChildren().addAll(pageselector,typeSelector, input, bold, italics, colour);
 	}
 	
 	private void inputUpdated(String text) {
